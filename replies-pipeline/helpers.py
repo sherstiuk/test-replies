0b2c5419bca25d77fd03e2cfc8035df6fe4e0c28
Anton Sherstiuk <anton.sherstiuk@epfl.ch>
2021-03-19 21:55:34 +0100

    helpers

---
+++ replies-pipeline/helpers.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+#from geopy.geocoders import Nominatim
+import logging
+
+from factcheckers import domains
+
+geolocator = Nominatim(user_agent="app")
+
+def condense_tweet(tw):
+    text = tw['extended_tweet']['full_text'] if tw['truncated'] else tw['text']
+
+    tweet_dict = {
+        'id': tw['id'],
+        'text': text,
+        'lang': tw['lang'],
+        #'reply_count': tw['reply_count'],
+        #'retweet_count': tw['retweet_count'],
+        #'favorite_count': tw['favorite_count'],
+        'created_at': datetime.strptime(tw['created_at'], '%a %b %d %H:%M:%S %z %Y'),
+        # 'is_quote_status': tw['is_quote_status'],
+        'user_verified': tw['user']['verified'],
+        'user_followers': tw['user']['followers_count'],
+        'country': get_country_code(tw)
+    }
+
+    return tweet_dict
+
+def condense_retweet(tw):
+    text = tw['retweeted_status']['extended_tweet']['full_text'] if tw['retweeted_status']['truncated'] else \
+    tw['retweeted_status']['text']
+
+    tweet_dict = {
+        'id': tw['id'],
+        'original_id': tw['retweeted_status']['id'],
+        'text': text,
+        'lang': tw['lang'],
+        #'reply_count': tw['reply_count'],
+        #'retweet_count': tw['retweet_count'],
+        #'favorite_count': tw['favorite_count'],
+        'created_at': datetime.strptime(tw['created_at'], '%a %b %d %H:%M:%S %z %Y'),
+        # 'is_quote_status': tw['is_quote_status'],
+        'user_verified': tw['user']['verified'],
+        'user_followers': tw['user']['followers_count'], # importance of user: verified, followers_count
+        'country': get_country_code(tw)
+    }
+
+    return tweet_dict
+
+def contains_factcheck_link(tweet):
+    if 'urls' in tweet:
+        if tweet['urls'] == []:
+            return False
+        else:
+            link = tweet['urls'][0]['expanded_url']
+            return any(domain in link for domain in domains)
+    else:
+        return False
+
+def contains_factcheck(tweet):
+    factcheck = False
+    if 'urls' in tweet and tweet['urls'] != []:
+        if contains_factcheck_link(tweet):
+            factcheck = True
+    return factcheck
+
+def get_link_quote(tw):
+    if 'quoted_status' not in tw:
+        return None
+    if 'extended_tweet' in tw['quoted_status']:
+        links = tw['quoted_status']['extended_tweet']['entities']['urls']
+    else:
+        links = tw['quoted_status']['entities']['urls']
+    if links == []:
+        return None
+    else:
+        return links[0]['expanded_url']
+
+def get_link_retweet(tw):
+    if 'extended_tweet' in tw['retweeted_status']:
+        links = tw['retweeted_status']['extended_tweet']['entities']['urls']
+    else:
+        links = tw['retweeted_status']['entities']['urls']
+    if links == []:
+        return None
+    else:
+        return links[0]['expanded_url']
+"""
+def get_country_code(tw):
+    address = tw['user']['location']
+    if address is None:
+        return None
+    try:
+        loc = geolocator.geocode(address)
+        if loc is None:
+            return None
+        code = geolocator.reverse((loc.raw['lat'], loc.raw['lon']), language='en').raw['address']['country_code']
+        return code
+    except Exception as exc:
+        logging.warning("Geocoding failed: %s" % exc)
+"""
